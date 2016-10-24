16337c33f7697ded859599905c8ceccbb4d23c7b
Michael Herman <hermanmu@gmail.com>
2016-10-24 15:12:02 -0600

    major updates yo

--- src/js/services.js
+++ src/js/services.js
@@ -10,12 +10,21 @@
 
   function coffeeService($http) {
     /*jshint validthis: true */
-    const baseURL = 'https://salty-inlet-28526.herokuapp.com/coffee/';
+    const baseURL = 'http://localhost:8000/coffee/';
     this.getAllCoffee = function() {
       return $http.get(baseURL);
     };
     this.getSingleCoffee = function(id) {
       return $http.get(`${baseURL}${id}`);
+    };
+    this.addCoffee = function(coffee) {
+      return $http({
+        method: 'POST',
+        url: baseURL,
+        data: coffee,
+        headers: {'Content-Type': 'application/json'}
+      });
+      // return $http.post(baseURL, coffee);
     };
   }
 

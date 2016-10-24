b37a0e08d37579b57450008e3504bab24b3fc0c5
Michael Herman <hermanmu@gmail.com>
2016-10-24 15:12:02 -0600

    major updates yo

--- js/components/coffee/coffee.controller.js
+++ js/components/coffee/coffee.controller.js
@@ -11,20 +11,53 @@
   function coffeeController($scope, coffeeService) {
     /*jshint validthis: true */
     const vm = this;
+    vm.form = false;
+    vm.coffeeObj = init();
+    vm.showForm = function() {
+      vm.form = true;
+    };
+    vm.addCoffee = function() {
+      coffeeService.addCoffee(vm.coffeeObj)
+      .then(() => {
+        coffeeService.getAllCoffee()
+        .then((coffees) => {
+          vm.coffee = coffees.data.data;
+        })
+        .catch((err) => {
+          console.log(err); // handle this error
+        });
+        vm.coffeeObj = init();
+        vm.form = false;
+      });
+    };
     coffeeService.getAllCoffee()
     .then((coffees) => {
       vm.coffee = coffees.data.data;
     })
     .catch((err) => {
-      console.log(err);
+      console.log(err); // handle this error
     });
-    coffeeService.getSingleCoffee(1)
-    .then((coffee) => {
-      console.log(coffee);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    // coffeeService.getSingleCoffee(1)
+    // .then((coffee) => {
+    //   console.log(coffee);
+    // })
+    // .catch((err) => {
+    //   console.log(err);
+    // });
+  }
+
+  function init() {
+    const coffeeObj = {
+      name: 'Chrismas Blend',
+      roaster: 'Sweet Bloom',
+      origin: 'Derek\'s backyard',
+      roast: 'Medium',
+      caffeine: 22,
+      decaf: 'false',
+      price: 888888,
+      quantity: 1
+    };
+    return coffeeObj;
   }
 
 })();
--- js/config.js
+++ js/config.js
@@ -6,16 +6,16 @@
     .module('myApp.config', [])
     .config(appConfig);
 
-    function appConfig($routeProvider) {
-      $routeProvider
-        .when('/coffee', {
-          templateUrl: 'js/components/coffee/coffee.view.html',
-          controller: 'coffeeController',
-          controllerAs: 'coffeeCtrl'
-        })
-        .otherwise({
-          redirectTo: '/coffee'
-        });
-    }
+  function appConfig($routeProvider) {
+    $routeProvider
+      .when('/coffee', {
+        templateUrl: 'js/components/coffee/coffee.view.html',
+        controller: 'coffeeController',
+        controllerAs: 'coffeeCtrl'
+      })
+      .otherwise({
+        redirectTo: '/coffee'
+      });
+  }
 
 })();
